2a42f635607b9d4c102b9544a10ca05c4d9289b3
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-05-29 17:03:46 -0400

    set up models, and json objects for seeds in database

--- models/blog.js
+++ models/blog.js
@@ -1,8 +1,41 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-class Blog extends Model {}
+// WHEN I click on an existing blog post
+// THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
 
+class Blog extends Model { }
 
+Blog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        blog_title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        blog_text: {
+            type: DataTypes.STRING,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "blog",
+    }
+);
 
 module.exports = Blog;
--- seeds/seed.js
+++ seeds/seed.js
@@ -16,12 +16,18 @@ const seedDatabase = async () => {
         returning: true,
     });
 
-    for (const blog of blogData) {
-        await Blog.create({
-            ...blog,
-            user_id: users[Math.floor(Math.random() * users.length)].id,
-        });
-    }
+    // for (const blog of blogData) {
+    //     await Blog.create({
+    //         //spreads blog object so sequelize can understand and use the data
+    //         ...blog,
+    //         user_id: users[Math.floor(Math.random() * users.length)].id,
+    //     });
+    // }
+
+    const blogs = await Blog.bulkCreate(blogData,{
+        individualHooks: true,
+        returning:true,
+    });
 
     await Comment.bulkCreate(commentData, {
         individualHooks: true,
