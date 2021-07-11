5cb5f9f95fb78a2f561ffc992f1beee1bd88f72d
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-07-11 16:24:10 -0400

    got home route to display seeded data on home page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,109 @@
-
 const router = require('express').Router();
 const { Blog, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', (req,res) =>{
-    res.render('homepage')
-
+router.get('/', (req, res) => {
+    Blog.findAll({
+        attributes: [ 'id', 'title', 'description', 'date_created' ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment', 'blog_id', 'user_id', 'date_created'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(blogData => {
+            const blogs = blogData.map(blog => blog.get({ plain: true }));
+            res.render('homepage', { blogs, logged_in: req.session.logged_in });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-
 router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
     res.render('login');
+});
+
+router.get('/blog/:id', (req, res) => {
+    Blog.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [ 'id', 'title', 'description', 'date_created' ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment', 'blog_id', 'user_id', 'date_created'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(blogData => {
+            if (!blogData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            const blog = blogData.get({ plain: true });
+            res.render('single-blog', { blog, logged_in: req.session.logged_in });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/blogs-comments', (req, res) => {
+    Blog.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [ 'id', 'title', 'description', 'date_created' ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment', 'blog_id', 'user_id', 'date_created'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(blogData => {
+            if (!blogData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            const blog = blogData.get({ plain: true });
+
+            res.render('blogs-comments', { blog, logged_in: req.session.logged_in });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
